be069f41717c524b9d0a4dfd7011c61d2f0f75e2
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-23 12:55:21 -0400

    Add sound and finish assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+var body = document.querySelector('body'),
+    lightSwitch = body.querySelector('button'),
+    text = body.querySelector('.status'),
+    audio = new Audio('./switch-3.mp3');
 
+lightSwitch.addEventListener('click', function(event) {
 
+  lightSwitch.classList.toggle('on');
+  lightSwitch.classList.toggle('off');
+  audio.play();
+
+  if (lightSwitch.classList.contains('off')) {
+    body.style.backgroundColor = "#222";
+    body.style.color = "#FFF";
+    text.innerText = "Hey, who turned off the lights?"
+  } else {
+    body.style.background = "#FFF";
+    body.style.color = "#222";
+    text.innerText = "It's so bright in here!";
+  }
+});
